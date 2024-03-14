7077b2a1413b23074f2dec343046973d4f0ec687
dawnsoapp <madi.jackson29@gmail.com>
2024-03-14 10:32:01 -0700

    doing a side by side of exercises to refresh on operators and coding semantics

--- Test.js
+++ Test.js
@@ -2,6 +2,16 @@
 // getting the Linux VM to connect to github
 
 let x = 5;
-let y = 7;
+let y = 10;
 let z = x + y;
-console.log(z)
+console.log(x *= y);
+console.log(z);
+
+let firstName = "John", lastName = "Doe", age = 35;
+console.log("His name is", firstName, lastName, "and he is", age, "years old.");
+
+const person = {
+    firstName: "Dawn",
+    lastName: "Soap"
+};
+console.log(person.firstName, person.lastName);
